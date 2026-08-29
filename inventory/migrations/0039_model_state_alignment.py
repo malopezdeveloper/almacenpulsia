216db62528af2cd63f51fd7f12d3b10d1c1b7272
malopezdeveloper <pulsiaworkersanlucaroffice@gmail.com>
2026-08-29 23:11:27 +0200

    Align migration state with current inventory models

---
+++ inventory/migrations/0039_model_state_alignment.py
@@ -0,0 +1,38 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('inventory', '0038_unit_work_queue_lifecycle'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='activesecuritysession',
+            options={'ordering': ('-last_activity', '-pk')},
+        ),
+        migrations.AlterModelOptions(
+            name='securityaccessevent',
+            options={'ordering': ('-created_at', '-pk')},
+        ),
+        migrations.AlterField(
+            model_name='activesecuritysession',
+            name='id',
+            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+        ),
+        migrations.AlterField(
+            model_name='component',
+            name='status',
+            field=models.CharField(choices=[('available', 'Disponible'), ('reserved', 'Reservado'), ('installed', 'Instalado'), ('rma', 'RMA'), ('scrapped', 'Merma')], db_index=True, default='available', max_length=20),
+        ),
+        migrations.AlterField(
+            model_name='securityaccessevent',
+            name='id',
+            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+        ),
+        migrations.AlterField(
+            model_name='securityaccesspolicy',
+            name='id',
+            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+        ),
+    ]
