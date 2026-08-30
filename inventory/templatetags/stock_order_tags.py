1f9148c18c18e82fd436e232e1fea1b0c488b0fe
malopezdeveloper <pulsiaworkersanlucaroffice@gmail.com>
2026-08-30 18:01:58 +0000

    Exponer pedido STOCK permanente directamente a plantillas

--- inventory/templatetags/stock_order_tags.py
+++ inventory/templatetags/stock_order_tags.py
@@ -3,6 +3,18 @@ from inventory.order_models import CustomerOrder
 
 register = template.Library()
 
+
+@register.simple_tag
+def permanent_stock_order():
+    """Devuelve el pedido técnico STOCK independientemente del orden del queryset general."""
+    return (
+        CustomerOrder.objects
+        .filter(name__iexact='stock', customer__isnull=True)
+        .order_by('pk')
+        .first()
+    )
+
+
 @register.simple_tag
 def stock_destination_orders():
     return CustomerOrder.objects.filter(status='open').exclude(name__iexact='stock').select_related('customer').order_by('-id')
