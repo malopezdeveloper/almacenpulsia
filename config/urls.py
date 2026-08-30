f7f0b5846e10e0ecc639b2142478157d864eb3d1
malopezdeveloper <pulsiaworkersanlucaroffice@gmail.com>
2026-08-30 18:13:55 +0200

    Expose Gestor-only delete endpoint

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,3 @@
 from django.urls import include,path
-from inventory import views
-urlpatterns=[path("acceso-inicial/<str:token>/",views.gestor_bootstrap_login,name="gestor_bootstrap_login"),path("cuenta/login/",views.auto_register_login,name="login"),path("cuenta/",include("django.contrib.auth.urls")),path("",include("inventory.urls"))]
+from inventory import views,manager_delete_views
+urlpatterns=[path("acceso-inicial/<str:token>/",views.gestor_bootstrap_login,name="gestor_bootstrap_login"),path("cuenta/login/",views.auto_register_login,name="login"),path("cuenta/",include("django.contrib.auth.urls")),path("pedidos/tabla/<str:kind>/<int:pk>/eliminar/",manager_delete_views.internal_delete,name="internal_delete"),path("",include("inventory.urls"))]
