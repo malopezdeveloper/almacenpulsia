0dada8046c3f75bdc4973d4e74dd18283c17b1f7
malopezdeveloper <pulsiaworkersanlucaroffice@gmail.com>
2026-08-30 19:05:25 +0200

    Exponer comprobación de prioridad de Pizarra

--- inventory/priority_urls.py
+++ inventory/priority_urls.py
@@ -3,6 +3,7 @@ from . import priority_views
 
 urlpatterns = [
     path('produccion/prioridades/', priority_views.priority_panel, name='board_priorities'),
+    path('produccion/prioridades/comprobar/', priority_views.priority_check, name='board_priority_check'),
     path('produccion/prioridades/<int:pk>/retirar/', priority_views.priority_disable, name='board_priority_disable'),
     path('pedidos/unidad/<int:unit_pk>/a-stock/', priority_views.move_unit_to_stock, name='move_unit_to_stock'),
 ]
