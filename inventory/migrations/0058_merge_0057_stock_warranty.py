13758458906a47f2262c55059df24a867ac67286
malopezdeveloper <pulsiaworkersanlucaroffice@gmail.com>
2026-09-05 23:03:25 +0000

    Merge parallel 0057 stock and warranty migrations

---
+++ inventory/migrations/0058_merge_0057_stock_warranty.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('inventory', '0057_stock_production_zone'),
+        ('inventory', '0057_warranty_permanent_order_role'),
+    ]
+
+    operations = []
